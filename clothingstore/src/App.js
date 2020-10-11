f2eb501cb505d70dbef3852fc3dcb6666a40235d
sarathy <p7080sty@gmail.com>
2020-10-11 08:03:07 +0530

    first commit for the project

---
+++ clothingstore/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Homepage from './homepage.component';
+function App() {
+  return (
+      <Homepage/>
+  );
+}
+
+export default App;
